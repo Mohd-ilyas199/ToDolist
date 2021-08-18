764b794f2c8f3895b969f01d60fe8539c3490507
mohd ilyas <ilyasfaizan8928@gmail.com>
2021-08-18 12:08:56 +0530

    Todolist practice

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+// import { Project } from './Components/Project';
+// import Logon from './Components/Logon';   
+// import Navs from './Components/Navigation/Navs';
+// import Nav from './Components/Navigation/Navs';
+// import { Home } from './Components/Home';
+// import { Project } from './Components/Project';
+// import { Placement2 } from './Components/Placement2';
+// import ParentPianomusic from './Components/ParentPianomusic';
+// import Audioplayer from './Components/Audioplayer';
+// import Calci from './Components/Calci';
+// import Fetchingemp from './Components/Redux/Fetchingemp';
+import { Home } from './Components/ToDoLists/Home';
+// import NewForm from './Components/NewForm';
+// import {Clock} from "./Components/Clock";
+function App() {
+  return (
+    <div className="App">
+     {/* <NewForm /> */}
+     {/* <Calci /> */}
+     {/* <ParentPianomusic /> */}
+     {/* <Audioplayer /> */}
+     {/* <Logon /> */}
+
+     {/* <Placement2 /> */}
+     {/* <Project /> */}
+     {/* <Home /> */}
+     {/* <Navs/> */}
+     {/* <Project/> */}
+     {/* <Clock /> */}
+     {/* <Fetchingemp /> */}
+     <Home/>
+    
+    </div>
+  );
+  }
+
+export default App;
